8dcb85359f158181b973be14ca1a28f5fac65c94
i3-code <i3pobox@gmail.com>
2021-03-06 15:57:11 +0300

    feat: country name on country's page

--- travel-app/src/components/Country/index.jsx
+++ travel-app/src/components/Country/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
 import { Grid, Typography } from '@material-ui/core';
 
 import Header from '../partials/Header';
 
 export default function Country() {
+  let { id } = useParams();
   return (
     <Grid>
-      <Header pageName="Country" />
-      <Typography variant="h3">Country</Typography>
+      <Header pageName={id} />
+      <Typography variant="h3">{id}</Typography>
     </Grid>
   );
 }
