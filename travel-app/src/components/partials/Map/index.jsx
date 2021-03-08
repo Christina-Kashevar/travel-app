9fd50c5d2db611be8aa28c8a12513902cbf91fab
i3-code <i3pobox@gmail.com>
2021-03-08 22:57:52 +0300

    feat: capital marker

--- travel-app/src/components/partials/Map/index.jsx
+++ travel-app/src/components/partials/Map/index.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useState} from 'react';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
-import ReactMapboxGl, { Layer, Source } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Source, Feature } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import useStyles from './styles';
 
@@ -8,7 +8,7 @@ import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
 
 import { MAPBOX_ACCESS_TOKEN } from '../../../data/constants';
-import { COUNTRY_COORDS } from '../../../data/geo';
+import { COUNTRY_COORDS, CAPITALS_COORDS } from '../../../data/geo';
 
 import { Grid, IconButton } from '@material-ui/core';
 
@@ -22,8 +22,16 @@ const COUNTRY_BONDS_SOURCE = {
 };
 
 const paint = {
-  'fill-color': '#fbc02d',
-  'fill-opacity': 0.5,
+  'fill-color': '#ff00ff',
+  'fill-opacity': 0.15,
+};
+
+const POSITION_CIRCLE_PAINT = {
+  'circle-stroke-width': 4,
+  'circle-radius': 10,
+  'circle-blur': 0.15,
+  'circle-color': '#3770C6',
+  'circle-stroke-color': 'white',
 };
 
 export default function Map(props) {
@@ -45,6 +53,7 @@ export default function Map(props) {
   };
 
   const filter = ['==', 'iso_3166_1', id.toUpperCase()];
+  const capitalCoords = CAPITALS_COORDS[id];
 
   return (
       <FullScreen handle={handleFs} onChange={trackFs}>
@@ -62,6 +71,9 @@ export default function Map(props) {
         >
           <Source id="country-bonds" tileJsonSource={COUNTRY_BONDS_SOURCE} />
           <Layer type="fill" sourceId="country-bonds" paint={paint} sourceLayer="country_boundaries" filter={filter}>
+          </Layer>
+          <Layer type="circle" paint={POSITION_CIRCLE_PAINT}>
+            <Feature coordinates={capitalCoords} />
           </Layer>
         </MapBox>
 
