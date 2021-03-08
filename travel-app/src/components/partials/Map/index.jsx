1b50a95f5c471cf312473abb2ab672456c22ebf7
i3-code <i3pobox@gmail.com>
2021-03-08 16:09:14 +0300

    refactor: fs button

--- travel-app/src/components/partials/Map/index.jsx
+++ travel-app/src/components/partials/Map/index.jsx
@@ -1,10 +1,11 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import useStyles from './styles';
 
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
 
 import { MAPBOX_ACCESS_TOKEN } from '../../../data/constants';
 import { Grid, IconButton } from '@material-ui/core';
@@ -13,43 +14,26 @@ const MapBox = ReactMapboxGl({
   accessToken: MAPBOX_ACCESS_TOKEN,
 });
 
-/*
-function polygons() {
-  map.addLayer(
-    {
-      id: 'country-boundaries',
-      source: {
-        type: 'vector',
-        url: 'mapbox://mapbox.country-boundaries-v1',
-      },
-      'source-layer': 'country_boundaries',
-      type: 'fill',
-      paint: {
-        'fill-color': '#d2361e',
-        'fill-opacity': 0.4,
-      },
-    },
-    'country-label'
-  );
-}
-*/
-
 export default function Map () {
-  const handle = useFullScreenHandle();
+  const handleFs = useFullScreenHandle();
+  const [fsState, setFsState] = useState(handleFs.active);
   const classes = useStyles();
 
+  const trackFs = useCallback((state) => setFsState(state), []);
+
   const toggleFs = () => {
-    if(handle.active) {
-      handle.exit();
+    if(handleFs.active) {
+      handleFs.exit();
       return;
     }
-    handle.enter();
+    handleFs.enter();
   };
+
   return (
-      <FullScreen handle={handle}>
+      <FullScreen handle={handleFs} onChange={trackFs}>
         <Grid className={classes.fsWrapper}>
           <IconButton onClick={toggleFs} className={classes.fsButton} color="primary">
-            <FullscreenIcon />
+            {(fsState) ? <FullscreenExitIcon /> : <FullscreenIcon />}
           </IconButton>
         </Grid>
         <MapBox
