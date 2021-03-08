ecc0bbb39f54ae302a1b07af04de5958208d12a7
i3-code <i3pobox@gmail.com>
2021-03-08 20:12:35 +0300

    refactor: contry swap eg->jp

--- travel-app/src/data/constants.js
+++ travel-app/src/data/constants.js
@@ -5,10 +5,10 @@ export const COUNTRY_DATA = [
   { id: 'mx', name: 'Mexico', capital: 'Mexico', timeZone: 'America/Mexico_City', currency: 'MXN' },
   { id: 'gr', name: 'Greece', capital: 'Athens', timeZone: 'Europe/Athens', currency: 'EUR' },
   { id: 'in', name: 'India', capital: 'Delhi', timeZone: 'Asia/Kolkata', currency: 'INR' },
-  { id: 'eg', name: 'Egypt', capital: 'Cairo', timeZone: 'Africa/Cairo', currency: 'EGP' },
+  { id: 'jp', name: 'Japan', capital: 'Tokyo', timeZone: 'Asia/Tokyo', currency: 'JPY' },
   { id: 'cn', name: 'China', capital: 'Beijing', timeZone: 'Asia/Shanghai', currency:'CNY' },
   { id: 'tr', name: 'Turkey', capital: 'Istanbul', timeZone: 'Europe/Istanbul', currency:'TRY' },
 ];
 
-export const COUNTRY_IDS = ['fr', 'it', 'ru', 'mx', 'gr', 'in', 'eg', 'cn', 'tr'];
-export const REACT_APP_WEATHER_KEY = '69671bc4a5a5ce9fec667fd138ce3060';
+export const COUNTRY_IDS = ['fr', 'it', 'ru', 'mx', 'gr', 'in', 'jp', 'cn', 'tr'];
+export const REACT_APP_WEATHER_KEY = 'c1b23d15ad387ce3fe178c7d2d465ccb';
