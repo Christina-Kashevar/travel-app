8d1b099a3097edfe1b9d2b96454f08f7fa7e3d9b
Christina Kashevar <christi_k@mail.ru>
2021-03-13 17:32:17 +0300

    refactor: check

--- travel-app/src/engine/index.js
+++ travel-app/src/engine/index.js
@@ -4,7 +4,7 @@ export function getCountryById(countryId, langId) {
   const result = COUNTRY_DATA.filter((item) => item.id === countryId)[0] || {};
   if (langId && langId !== 'en-US') {
     const lang = langId.slice(0, 2);
-    const dataPatch = (result.translations) ? result.translations[lang] || {};
+    const dataPatch = (result.translations) ? result.translations[lang] : {};
 
     return { ...result, ...dataPatch };
   }
